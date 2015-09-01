af0969d522e69fa2edce725c3f840d6b04a4ca60
Taras Serdyuk <taras@techburg.io>
2015-09-01 15:48:07 +0300

    basic serving

--- ferad/src/main.js
+++ ferad/src/main.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
 import rc from 'rc'
+import serve from './serve'
 import Locate from './locate'
 
 const options = rc('ferad', {
-	config: 'ferad.json',
-	path: ['~']
+	appConfig: 'app.json',
+	path: ['~'],
+	port: 5000
 })
-const locate = Locate(options.config, options.path)
-locate('test', console.log)
+const locate = Locate(options.appConfig, options.path)
+
+serve(options.port, (project) => {
+	locate(project, console.log)
+})

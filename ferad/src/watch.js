4fbe9a1556fbf0e4bce6adda4a53ecfa546e5d44
Taras Serdyuk <taras@techburg.io>
2015-09-03 04:41:06 +0300

    renamed lib to dist

--- ferad/src/watch.js
+++ ferad/src/watch.js
@@ -4,8 +4,8 @@ import { build, misc } from 'ferad-tasks'
 
 export default function watch(cwd, port) {
 	gulp.task('default', ['serve', 'watch-jade'])
-	gulp.task('serve', ()=> misc.serve({ port, root: 'dist/1', livereload: true }))
+	gulp.task('serve', ()=> misc.serve({ port, root: 'target/1', livereload: true }))
 	gulp.task('watch-jade', ['jade'], ()=> gulp.watch('src/*.jade', { cwd }, ['jade']))
-	gulp.task('jade', ()=> build.jade({ src: 'src/*.jade', dest: 'dist/1', cwd }))
+	gulp.task('jade', ()=> build.jade({ src: 'src/*.jade', dest: 'target/1', cwd }))
 	run('default')
 }

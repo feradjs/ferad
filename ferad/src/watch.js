34087eb2f0354c8738a67f6eed0b732de654f700
Taras Serdyuk <taras@techburg.io>
2015-09-03 04:51:07 +0300

    more demo with css

--- ferad/src/watch.js
+++ ferad/src/watch.js
@@ -2,9 +2,12 @@ import gulp from 'gulp'
 import { build, misc } from 'ferad-tasks'
 
 export default function watch(cwd, port) {
-	gulp.task('default', ['serve', 'watch-jade'])
+	gulp.task('default', ['serve', 'watch'])
 	gulp.task('serve', ()=> misc.serve({ port, root: 'target/1', livereload: true }))
-	gulp.task('watch-jade', ['jade'], ()=> gulp.watch('src/*.jade', { cwd }, ['jade']))
-	gulp.task('jade', ()=> build.jade({ src: 'src/*.jade', dest: 'target/1', cwd }))
+	gulp.task('watch', ['watch-jade', 'watch-sass'])
+	gulp.task('watch-jade', ['jade'], ()=> gulp.watch('*.jade', { cwd }, ['jade']))
+	gulp.task('watch-sass', ['sass'], ()=> gulp.watch('*.{scss,css}', { cwd }, ['sass']))
+	gulp.task('jade', ()=> build.jade({ src: '*.jade', dest: 'target/1', plumber: true, cwd, }))
+	gulp.task('sass', ()=> build.sass({ src: '*.{scss,css}', dest: 'target/1', plumber: true, cwd}))
 	gulp.start('default')
 }

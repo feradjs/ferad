460358f4bc8c607dcc5903846e55af43dcb27c68
Taras Serdyuk <taras@techburg.io>
2015-09-03 04:42:20 +0300

    direct gulp task invocation

--- ferad/src/watch.js
+++ ferad/src/watch.js
@@ -1,5 +1,4 @@
 import gulp from 'gulp'
-import run from 'run-sequence'
 import { build, misc } from 'ferad-tasks'
 
 export default function watch(cwd, port) {
@@ -7,5 +6,5 @@ export default function watch(cwd, port) {
 	gulp.task('serve', ()=> misc.serve({ port, root: 'target/1', livereload: true }))
 	gulp.task('watch-jade', ['jade'], ()=> gulp.watch('src/*.jade', { cwd }, ['jade']))
 	gulp.task('jade', ()=> build.jade({ src: 'src/*.jade', dest: 'target/1', cwd }))
-	run('default')
+	gulp.start('default')
 }

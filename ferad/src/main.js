3c059cc429d34ab020e82819cb139821a8a91af4
Taras Serdyuk <taras@techburg.io>
2015-09-09 17:03:09 +0300

    #14 done

--- ferad/src/main.js
+++ ferad/src/main.js
@@ -2,14 +2,17 @@
 require('babel/polyfill')
 
 import rc from 'rc'
+import json from 'jsonfile'
 import execute from './execute'
 
 const env = rc('ferad', {
-	appConfig: 'app.json',
 	configurator: './configurator',
+	appConfig: 'app.json',
+	dest: 'dest',
 	port: 5000
 })
+const app = json.readFileSync(env.appConfig, { throws: false })
 const configurator = require(env.configurator)
-const tasks = configurator(env, null, process.cwd())
+const tasks = configurator(env, app, process.cwd())
 
 execute(tasks)

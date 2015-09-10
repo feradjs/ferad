0a518315ee24a8bcbd043c6f77112f9b38b6cf5d
Taras Serdyuk <taras@techburg.io>
2015-09-10 18:00:48 +0300

    clean task + api change

--- tasks/src/actions.js
+++ tasks/src/actions.js
@@ -1,11 +1,15 @@
 import gulp from 'gulp'
+import del from 'del'
 import _ from './common'
 
 export default {
+	clean({ dest }, cb) {
+		del(dest, { force: true }, cb)
+	},
 	serve(o) {
 		_.connect.server(o)
 	},
-	watch({ src, task, cwd }, gulp) {
+	watch({ src, task, cwd }, cb, gulp) {
 		_.watch(src, { cwd }, _.batch(
 			(events, done) => gulp.start(task, done)
 		))

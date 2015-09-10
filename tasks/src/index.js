c1e1a7a09094fe5127b6144854a23f0cbe916270
Taras Serdyuk <taras@techburg.io>
2015-09-10 16:36:26 +0300

    rename
    --HG--
    rename : src/misc.js => src/actions.js
    rename : src/build.js => src/resources.js

--- tasks/src/index.js
+++ tasks/src/index.js
@@ -1,4 +1,4 @@
-import build from './build'
-import misc from './misc'
+import actions from './actions'
+import resources from './resources'
 
-export default Object.assign({}, build, misc)
+export default Object.assign({}, actions, resources)

0b22d1e6ba2ba7e1547a26811793cb3ab04aeefa
Taras Serdyuk <taras@techburg.io>
2015-09-10 16:54:31 +0300

    minor

--- tasks/src/resources.js
+++ tasks/src/resources.js
@@ -1,12 +1,13 @@
-import gulp from 'gulp'
 import _ from './common'
 
 export default {
 	jade(o) {
-		return _.emit(o, _.src(o).pipe(_.jade()))
+		return _.emit(o, _.src(o)
+			.pipe(_.jade()))
 	},
 	sass(o) {
-		return _.emit(o, _.src(o).pipe(_.sass())
+		return _.emit(o, _.src(o)
+			.pipe(_.sass())
 			.pipe(_.minifyCss()))
 	}
 }

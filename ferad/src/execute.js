ca47f2877ecb7a4d811c90b2caee297257d5f649
Taras Serdyuk <taras@techburg.io>
2015-09-10 18:01:34 +0300

    async task api

--- ferad/src/execute.js
+++ ferad/src/execute.js
@@ -2,8 +2,8 @@ import gulp from 'gulp'
 
 export default function execute(tasks) {
 	tasks.forEach(({ name, modul, func, options, depends }) =>
-		gulp.task(name, depends, () =>
-			require(modul)[func](options, gulp)
+		gulp.task(name, depends, (cb) =>
+			require(modul)[func](options, cb, gulp)
 		)
 	)
 	gulp.start('default')

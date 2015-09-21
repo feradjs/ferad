41296367c263104e07508077bca280db844d9fa3
Taras Serdyuk <taras@techburg.io>
2015-09-21 04:39:42 +0300

    #38 workaround

--- tasks/src/common.js
+++ tasks/src/common.js
@@ -1,13 +1,10 @@
 import gulp from 'gulp'
 import plugins from 'gulp-load-plugins'
-import u from 'ferad-utils'
 
 function src({ src, cwd, plumber = false }) {
-	return (plumber ? plumb : u.id)
-		(gulp.src(src, { cwd }))
-}
-function plumb(stream) {
-	return stream.pipe(_.plumber())
+	return gulp.src(src, { cwd })
+		.pipe(_.plumber(plumber ?
+			null : { errorHandler: fail }))
 }
 function emit(o, stream) {
 	return dest(o, stream)
@@ -17,11 +14,15 @@ function dest({ dest }, stream) {
 	return stream.pipe(gulp.dest(dest))
 }
 
+function fail(error) {
+	console.error(error.toString())
+	process.exit(1)
+}
 function logError(error) {
 	_.util.log('[' + _.util.colors.red('ERROR') + ']', error.message)
 	this.emit('end')
 }
 
 const _ = plugins()
-Object.assign(_, { src, emit, dest, logError })
+Object.assign(_, { src, emit, dest, fail, logError })
 export default _

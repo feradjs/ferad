778c59d3c48022f66833e6a169b1522c8fb9507b
Taras Serdyuk <taras@techburg.io>
2015-10-17 10:32:48 +0300

    #58 option groups

--- ferad/src/pipeline.js
+++ ferad/src/pipeline.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 
 export default function pipeline(command, config) {
 	const sequence = command
@@ -7,11 +8,17 @@ export default function pipeline(command, config) {
 		sequence,
 		tasks: sequence.map(task => {
 			const [func, ...buckets] = task.split(':')
-			buckets.unshift('default')
+			buckets.unshift(':default')
 			const options = Object.assign.apply(null,
-				buckets.map(bucket => config[':' + bucket])
+				_.flattenDeep(getBuckets(buckets.join(':')))
 			)
 			return { name: task, func, options }
+		})
+	}
+	function getBuckets(names) {
+		return names.split(':').slice(1).map(name => {
+			const bucket = config[':' + name]
+			return _.isString(bucket) ? getBuckets(bucket) : bucket
 		})
 	}
 }
--- ferad/test/pipeline.js
+++ ferad/test/pipeline.js
@@ -41,7 +41,35 @@ describe('pipeline', () => {
 				task('a:1:2:3', 'a', { x: 3, y: 2, z: 1 })
 			]
 		)
-		// Nested
+		test('alias',
+			'a:x', {
+				':x': ':y',
+				':y': { value: 1 },
+			}, ['a:x'], [
+				task('a:x', 'a', { value: 1 })
+			]
+		)
+		test('group',
+			'a:g', {
+				':default': { x: 0, y: 0, z: 0 },
+				':1': { x: 1, y: 1 },
+				':2': { x: 2 },
+				':g': ':1:2'
+			}, ['a:g'], [
+				task('a:g', 'a', { x: 2, y: 1, z: 0 })
+			]
+		)
+		test('nested group',
+			'a:g', {
+				':1': { x: 1 },
+				':2': { y: 2 },
+				':3': { z: 3 },
+				':j': ':1:2',
+				':g': ':j:3'
+			}, ['a:g'], [
+				task('a:g', 'a', { x: 1, y: 2, z: 3 })
+			]
+		)
 		// Error
 	})
 })

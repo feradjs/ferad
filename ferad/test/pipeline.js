bbad083f36cbb0ab81a8992c59ef4864e77dc4e7
Taras Serdyuk <taras@techburg.io>
2015-10-17 10:55:48 +0300

    #58 option tests refactoring

--- ferad/test/pipeline.js
+++ ferad/test/pipeline.js
@@ -10,69 +10,62 @@ describe('pipeline', () => {
 		]
 	)
 	describe('options', () => {
-		test('default',
-			'a', {
+		testOptions('default',
+			'', {
 				':default': { value: 0 }
-			}, ['a'], [
-				task('a', 'a', { value: 0 })
-			]
+			}, { value: 0 }
 		)
-		test('single',
-			'a:1', {
+		testOptions('single',
+			':1', {
 				':1': { value: 1 },
-			}, ['a:1'], [
-				task('a:1', 'a', { value: 1 })
-			]
+			}, { value: 1 }
 		)
-		test('defaulted',
-			'a:1', {
+		testOptions('defaulted',
+			':1', {
 				':default': { x: 0, y: 0 },
 				':1': { x: 1},
-			}, ['a:1'], [
-				task('a:1', 'a', { x: 1, y: 0 })
-			]
+			}, { x: 1, y: 0 }
 		)
-		test('multiple',
-			'a:1:2:3', {
+		testOptions('multiple',
+			':1:2:3', {
 				':1': { x: 1, y: 1, z: 1 },
 				':2': { x: 2, y: 2 },
 				':3': { x: 3 }
-			}, ['a:1:2:3'], [
-				task('a:1:2:3', 'a', { x: 3, y: 2, z: 1 })
-			]
+			}, { x: 3, y: 2, z: 1 }
 		)
-		test('alias',
-			'a:x', {
+		testOptions('alias',
+			':x', {
 				':x': ':y',
 				':y': { value: 1 },
-			}, ['a:x'], [
-				task('a:x', 'a', { value: 1 })
-			]
+			}, { value: 1 }
 		)
-		test('group',
-			'a:g', {
+		testOptions('group',
+			':g', {
 				':default': { x: 0, y: 0, z: 0 },
 				':1': { x: 1, y: 1 },
 				':2': { x: 2 },
 				':g': ':1:2'
-			}, ['a:g'], [
-				task('a:g', 'a', { x: 2, y: 1, z: 0 })
-			]
+			}, { x: 2, y: 1, z: 0 }
 		)
-		test('nested group',
-			'a:g', {
+		testOptions('nested group',
+			':g', {
 				':1': { x: 1 },
 				':2': { y: 2 },
 				':3': { z: 3 },
 				':j': ':1:2',
 				':g': ':j:3'
-			}, ['a:g'], [
-				task('a:g', 'a', { x: 1, y: 2, z: 3 })
-			]
+			}, { x: 1, y: 2, z: 3 }
 		)
 		// Error
 	})
 })
+
+function testOptions(name, options, config, result) {
+	const command = 'task' + options
+	test(name, command, config, [command],
+		[task(command, 'task', result)]
+	)
+}
 
 function test(name, command, config, sequence, tasks) {
 	it(name, () => deepEqual(

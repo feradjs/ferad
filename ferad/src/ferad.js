a58d33a185b7ed2501417fed12f72ba71c6daed0
Taras Serdyuk <taras@techburg.io>
2015-10-17 07:26:14 +0300

    2.0 start

--- ferad/src/ferad.js
+++ ferad/src/ferad.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 require('babel/polyfill')
 
-import rc from 'rc'
-import json from 'jsonfile'
 import gulp from 'gulp'
+import json from 'jsonfile'
 import gulpLog from './gulp-log'
-import execute from './execute'
-
-const env = rc('ferad', require('../ferad.json'))
-const app = json.readFileSync(env.appConfig, { throws: false })
-const configurator = require(env.configurator)
-const tasks = configurator(env, app, process.cwd())
-const task = env._[0] || 'default'
+import old from './old/ferad'
 
 gulpLog(gulp)
-execute(task, tasks)
+
+const config = json.readFileSync('ferad.json', { throws: false })
+if (config) {
+	// 2.0
+} else {
+	old()
+}
---
+++ ferad/src/old/ferad.js
@@ -0,0 +1,12 @@
+import rc from 'rc'
+import json from 'jsonfile'
+import execute from './execute'
+
+export default function ferad() {
+	const env = rc('ferad', require('../../ferad-old.json'))
+	const app = json.readFileSync(env.appConfig, { throws: false })
+	const configurator = require(env.configurator)
+	const tasks = configurator(env, app, process.cwd())
+	const task = env._[0] || 'default'
+	execute(task, tasks)
+}

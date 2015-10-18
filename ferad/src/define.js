430810cc7276cebeb3420795d5b8eb832e3a3122
Taras Serdyuk <taras@techburg.io>
2015-10-18 08:02:38 +0300

    2.0 execution

---
+++ ferad/src/define.js
@@ -0,0 +1,24 @@
+import gulp from 'gulp'
+import run from 'run-sequence'
+import shell from 'gulp-shell'
+import tasks from 'ferad-tasks'
+
+export default function define(tasks) {
+	tasks.forEach(task =>
+		gulp.task(task.name, cb =>
+			func[task.type](task, cb)
+		)
+	)
+}
+
+const func = {
+	sequence({ tasks }, cb) {
+		run.apply(null, tasks.concat([cb]))
+	},
+	task({ func, options }, cb) {
+		tasks[func](options, cb, gulp)
+	},
+	shell({ commands }) {
+		return shell.task(commands)()
+	}
+}
--- ferad/src/ferad.js
+++ ferad/src/ferad.js
@@ -4,13 +4,16 @@ require('babel/polyfill')
 import gulp from 'gulp'
 import json from 'jsonfile'
 import gulpLog from './gulp-log'
+import pipeline from './pipeline'
+import define from './define'
 import old from './old/ferad'
 
 gulpLog(gulp)
 
 const config = json.readFileSync('ferad.json', { throws: false })
 if (config) {
-	// 2.0
+	define(pipeline(process.argv[2], config))
+	gulp.start('default')
 } else {
 	old()
 }

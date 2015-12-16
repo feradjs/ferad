db4160de7e778fc163eaffe21c7cca908589bfab
Taras Serdyuk <taras@techburg.io>
2015-12-17 02:20:25 +0300

    general tasks

--- packages/ferad-tasks/src/index.js
+++ packages/ferad-tasks/src/index.js
@@ -1,5 +1,32 @@
-import commands from './commands'
-import resources from './resources'
-import scripts from './scripts'
+import gulp from 'gulp'
+import rsync from 'rsync'
 
-export default Object.assign({}, commands, resources, scripts)
+module.exports = {
+	copy({ src, dest }) {
+		return gulp.src(src)
+			.pipe(gulp.dest(dest))
+	},
+	clean({ dest }) {
+		return del(dest, { force: true })
+	},
+	serve(o) {
+		require('live-server').start(o)
+	},
+	watch({ src, task }, cb, gulp) {
+		_.watch(src, () =>
+			gulp.start(task)
+		)
+	},
+	rsync({ target, dest }, cb) {
+		new Rsync()
+			.archive()
+			.delete()
+			.compress()
+			.progress()
+			.source(dest + '/')
+			.destination(target)
+			.execute((error, code, cmd) => { cb(error) },
+				data => console.log(data.toString('utf8'))
+			)
+	}
+}

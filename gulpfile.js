9c1ea65b19d16ed0ddaee1f13705f3fb73f7cd6f
Taras Serdyuk <taras@techburg.io>
2015-12-16 21:16:11 +0300

    gulp basic test

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,30 @@ var _ = require('gulp-load-plugins')()
 
 var dest = 'packages'
 var scripts = 'packages/*/src/**/*.js'
+var tests = 'packages/*/test/**/*.js'
 
 gulp.task('default', ['compile'], function() {
-	_.watch(scripts, function() {
+	_.watch([scripts, tests], function() {
 		gulp.start('compile')
 	})
 })
 
 gulp.task('compile', function() {
-	gulp.src(scripts)
+	return gulp.src(scripts)
 		.pipe(_.errorNotifier())
 		.pipe(through.obj(function(file, enc, callback) {
 			file.path = file.path.replace('src', 'dist')
 			callback(null, file)
 		}))
+		// TODO: clean on first run
 		.pipe(_.newer(dest))
-		.pipe(_.babel())
+		.pipe(_.babel({ presets: ['es2015'] }))
 		.pipe(gulp.dest(dest))
 
+})
+
+gulp.task('test', function() {
+	return gulp.src('packages/*/dist-test/**/*.js')
+		.pipe(_.babel({ presets: ['es2015'] }))
+		.pipe(_.mocha())
 })

64922883bdf799b78694fec938a9b24de6dc0d08
Taras Serdyuk <taras@techburg.io>
2015-12-17 01:02:21 +0300

    clean-up

--- packages/ferad-tasks/src/commands.js
+++ packages/ferad-tasks/src/commands.js
@@ -1,11 +1,8 @@
 import gulp from 'gulp'
 import del from 'del'
 import Rsync from 'rsync'
-import mustache from 'mustache'
 import fs from 'fs'
 import _ from './common'
-
-mustache.escape = data => data
 
 export default {
 	// TODO: copy task
@@ -36,11 +33,5 @@ export default {
 			.execute((error, code, cmd) => { cb(error) },
 				data => console.log(data.toString('utf8'))
 			)
-	},
-	template(data, cb) {
-		const path = require.resolve('../templates/' + data.name)
-		const template = fs.readFileSync(path, { encoding: 'utf8' })
-		const output = mustache.render(template, data)
-		fs.writeFile(data.name, output, cb)
 	}
 }

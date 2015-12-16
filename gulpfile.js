f4fb04cb6c6475a64c54d3dcaf07f5087ac5d5aa
Taras Serdyuk <taras@techburg.io>
2015-12-16 21:39:38 +0300

    gulp clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 var gulp = require('gulp')
+var run = require('run-sequence')
 var through = require('through2')
 var _ = require('gulp-load-plugins')()
+var del = require('del')
 
 var dest = 'packages'
 var scripts = 'packages/*/src/**/*.js'
 var tests = 'packages/*/test/**/*.js'
 
-gulp.task('default', ['compile'], function() {
+gulp.task('default', ['build'], function() {
 	_.watch([scripts, tests], function() {
 		gulp.start('compile')
 	})
+})
+
+gulp.task('build', function(cb) {
+	run('clean', 'compile', cb)
+})
+
+gulp.task('clean', function() {
+	return del('packages/*/dist')
 })
 
 gulp.task('compile', function() {
@@ -19,15 +29,13 @@ gulp.task('compile', function() {
 			file.path = file.path.replace('src', 'dist')
 			callback(null, file)
 		}))
-		// TODO: clean on first run
 		.pipe(_.newer(dest))
 		.pipe(_.babel({ presets: ['es2015'] }))
 		.pipe(gulp.dest(dest))
-
 })
 
 gulp.task('test', function() {
-	return gulp.src('packages/*/dist-test/**/*.js')
+	return gulp.src(tests)
 		.pipe(_.babel({ presets: ['es2015'] }))
 		.pipe(_.mocha())
 })
